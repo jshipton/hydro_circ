4f36c13190a57f22192708b4152adafc447fa1a7
jshipton <j.shipton@exeter.ac.uk>
2025-07-02 14:37:38 +0100

    add plotting script and tidier version of hydro_circ

--- hydro_circ.py
+++ hydro_circ.py
@@ -1,67 +1,61 @@
-import matplotlib.pyplot as plt
-from matplotlib import cm
 
+
+# Import specific functions from Firedrake
 from firedrake import (SpatialCoordinate, TestFunction, TrialFunction, norm,
                        Function, dx, lhs, rhs, inner, LinearVariationalProblem,
-                       LinearVariationalSolver, Constant, acos, exp, trisurf,
-                       tricontourf, functionspaceimpl)
+                       LinearVariationalSolver, Constant, acos, exp,
+                       jump, dot, FacetNormal, avg, grad)
 from gusto import *
+from plotting import *
 
 
-def plot_field(field):
-    # function to plot the field and show the plot - could be amended
-    # to save a figure instead and other plotting commands can be added
-    tsurf = trisurf(field)
-    plt.colorbar(tsurf)
-    plt.title(field.name())
-    plt.show()
+# =======================================================================
+# Planetary parameters relevant to Proxima B.
+R = 7160000.        # radius of planet (m)
+Omega = 6.501e-6    # rotation rate of planet (rad s^-1)
+g = 10.9            # gravitational acceleration (m s^-2)
+H = 2500.           # depth of atmospheric boundary layer (mean fluid depth) (m)
 
+# =======================================================================
+# Coefficients used in equations
+r = 3.6e-5          # linear coefficient of friction (N kg^-1 m^-1 s)
+alpha = 1e-5
 
-def plot_field_latlon(field):
-    # DON'T USE ME!!
-    # function to plot the field and show the plot - could be amended
-    # to save a figure instead and other plotting commands can be added
-    mesh_ll = get_flat_latlon_mesh(field.function_space().mesh())
-    field_ll = Function(
-        functionspaceimpl.WithGeometry.create(field.function_space(), mesh_ll),
-        val=field.topological,
-        name=field.name()
-    )
-    tsurf = tricontourf(field_ll)
-    plt.colorbar(contourf)
-    plt.title(field_ll.name())
-    plt.show()
+# =======================================================================
+# Large scale moisture parameters
+q_ut = 0.004        # upper-tropospheric specific humidity (kg kg^-1)
+qW = q_ut           # tropically averaged descending moisture (kg
+                    # kg^-1). q_ut for now, but should be somewhat
+                    # higher probably.
 
+# =======================================================================
+# Parameters used to compute evaporation and precipitation
+rho0 = 1                     # constant density of boundary layer air (kg m^-3)
+cH = 1e-3                    # bulk coefficient (no units)
+L = 2.5e6                    # latent heat of condensation (J kg^-1)
+Rw = 416                     # specific gas constant for water vapour
+                             # (J K^-1 kg^-1)
+e0 = 2300*exp(L/(Rw*293.))   # reference saturation vapour pressure (Pa)
+p0 = 1e5                     # reference pressure (Pa)
+mB = 0.006029                # boundary layer overturning timescale
+                             # (kg m^-2 s^-1)
+                    
+# =======================================================================
+# Parameters used to compute vertical velocity w
+Cp = 1005.                   # specific heat capacity at constant
+                             # pressure of dry air (J K^-1 kg^-1)
+dtheta = 50                  # change in potential temperature across
+                             # troposphere (K)
+Qcl = 100.                   # net combined radiative-sensible cooling
+                             # of free atmosphere (W m^-2)
+qC = 0.018                   # critical specific humidity for
+                             # initiation of convection (kg kg^-1)
 
-def print_minmax(field):
-    # function to print the min and max of field in a nice way
-    print(f"min and max of {field.name()}: {field.dat.data.min()}, {field.dat.data.max()}")
-
-
-# Here are all the physical constants
-R = 7160000.                 # radius of planet (m)
-Omega = 6.501e-6             # rotation rate of planet
-g = 10.9
-H = 2500.
-r = 3.6e-5
-alpha = 500.
-L = 2.5e6
-Rw = 416
-p0 = 1e5
-e0 = 2300*exp(L/(Rw*293.))
-q_ut = 0.004
-mB = 0.006029
-qW = q_ut
-rho0 = 1
-cH = 1e-3
-Cp = 1005.
-dtheta = 50
-qC = 0.018
-Qcl = 100.
-
-# set up mesh, timestep and use the Gusto domain function to set up
-# the finite element function spaces
-mesh = GeneralCubedSphereMesh(radius=R, num_cells_per_edge_of_panel=16,
+# =======================================================================
+# Set up mesh, timestep and use Gusto to set up the finite element
+# function spaces.
+ncells = 16     # number of cells along the edge of each cube face
+mesh = GeneralCubedSphereMesh(radius=R, num_cells_per_edge_of_panel=ncells,
                               degree=2)
 dt = 100
 domain = Domain(mesh, dt, family="RTCF", degree=1)
@@ -81,9 +75,13 @@ w = Function(Vdg, name="w")       # vertical velocity
 h = Function(Vdg, name='h')       # depth perturbation
 u = Function(Vu, name='u')        # velocity
 q = Function(Vdg, name='q')       # moisture
-qA = Function(Vdg, name='qA')     # 
+qA = Function(Vdg, name='qA')     #
 E = Function(Vdg, name='E')       # evaporation
 P = Function(Vdg, name='P')       # precipitation
+
+
+# Now we setup functions for the Coriolis parameter, surface
+# temperature and saturation function.
 
 # Coriolis
 fexpr = 2*Omega*sin(lat)
@@ -93,26 +91,40 @@ f = Function(Vcg).interpolate(fexpr)
 # perturbation centered on (lon_c, lat_c)
 lon_c = 0
 lat_c = 0
-Tmin = 155
-Tmax = 160
+Tmin = 230
+Tpert = 80
 
 def d(lon1, lat1, lon2, lat2):
     # returns distance on sphere between (lon1, lat1) and (lon2, lat2)
     return acos(sin(lat1)*sin(lat2) + cos(lat1)*cos(lat2)*cos(lon1-lon2))
 
 Ts = Function(Vdg, name='Ts')
-Ts.interpolate(Tmin + Tmax * exp(-d(lon_c, lat_c, lon, lat)**2))
+Ts.interpolate(Tmin + Tpert * exp(-d(lon_c, lat_c, lon, lat)**2/0.1))
 
 # saturation function
 qs = 0.622 * e0 * exp(-L/(Rw*Ts)) / p0
+plot_field(Ts, "Ts")
 
-
+# =======================================================================
+# Now set up the finite element form of the equations we are going to solve.
 # =======================================================================
 # setup the h equation and solver
 test_h = TestFunction(Vdg)
 trial_h = TrialFunction(Vdg)
 h0 = Function(Vdg)
-h_eqn = test_h * trial_h * dx + dt * test_h * (-w + h0/alpha) * dx
+n = FacetNormal(mesh)
+dx_max = 2*pi*R/(4*ncells)
+mu = 10/dx_max
+alpha = 0.00001
+h_eqn = test_h * (trial_h - h0) * dx + dt * (
+    - test_h * w * dx
+    + (1/alpha) * (
+        inner(grad(test_h), grad(h0)) * dx
+        - inner(2*avg(h0 * n), avg(grad(test_h))) * dS
+        - inner(avg(grad(h0)), 2*avg(test_h * n)) * dS
+        + mu * inner(2*avg(h0 * n), 2*avg(test_h * n)) * dS
+    )
+)
 h_lhs = lhs(h_eqn)
 h_rhs = rhs(h_eqn)
 h_prob = LinearVariationalProblem(h_lhs, h_rhs, h)
@@ -124,8 +136,8 @@ h_solver = LinearVariationalSolver(h_prob)
 test_u = TestFunction(Vu)
 trial_u = TrialFunction(Vu)
 u0 = Function(Vu)
-u_eqn = inner(test_u, trial_u) * dx + dt * (
-    inner(test_u, f*domain.perp(u) + r*u0) * dx - g * div(test_u) * h * dx
+u_eqn = inner(test_u, (trial_u - u0)) * dx + dt * (
+    inner(test_u, f*domain.perp(u0) + r*u0) * dx + g * div(test_u) * h * dx
 )
 u_lhs = lhs(u_eqn)
 u_rhs = rhs(u_eqn)
@@ -138,29 +150,48 @@ u_solver = LinearVariationalSolver(u_prob)
 test_q = TestFunction(Vdg)
 trial_q = TrialFunction(Vdg)
 q0 = Function(Vdg)
-q_eqn = test_q * trial_q * dx + dt * test_q * ((q0 - qA) * div(u) - E + P) * dx
+un = 0.5*(dot(u, n) + abs(dot(u, n)))
+q_eqn = test_q * (trial_q - q0) * dx + dt * (
+    - div(test_q * u) * q0 * dx
+    + dot(jump(test_q), (un('+')*q0('+') - un('-')*q0('-'))) * dS
+    + test_q * ((q0 - qA) * div(u)
+    - (E - P)/(rho0*H)) * dx
+    )
 q_lhs = lhs(q_eqn)
 q_rhs = rhs(q_eqn)
 q_prob = LinearVariationalProblem(q_lhs, q_rhs, q)
 q_solver = LinearVariationalSolver(q_prob)
 
-
 # =======================================================================
-# symbolic expressions for calculating various fields
-w_expr = (L * P - Qcl) / (H * rho0 * Cp * dtheta)
+# Now we define some expressions for calculating various fields -
+# hopefully the naming is obvious: e.g. `E_expr` is the expression
+# used to calculate the evaporation `E`."""
+w_expr = (L * P - Qcl) / (rho0 * Cp * dtheta)
 qA_expr = conditional(w < 0, qW, 0)
-E_expr = rho0 * cH * sqrt(dot(u, u)) * (qs - q)
-P_expr = conditional(q > qC, mB * (q - q_ut), 0)
-
+E_expr = conditional(qs > q0, rho0 * cH * sqrt(dot(u, u)) * (qs - q), 0)
+P_expr = conditional(q0 > qC, mB * (q0 - q_ut), 0)
 
 # =======================================================================
-# initial conditions:
+# Our initial conditions are that the height perturbation is zero
+# (i.e. the depth of the atmosphere is H) and the horizontal velocity
+# is zero. The initial water vapour is 0.7 times the saturation
+# function. Where this is above `qC` there will be precipitation, and
+# hence vertical velocity, so we compute `P` and then `w`.
+
 # initial water vapour, q, is 0.7 * saturation value
-q.interpolate(0.7 * qs)
+q0.interpolate(0.7 * qs)
+q_init = Function(q0.function_space()).assign(q0)
+print_minmax(q0)
+print(qC)
 # compute P from initial q
 P.interpolate(P_expr)
+print_minmax(P)
 # compute initial w
 w.interpolate(w_expr)
+
+plot_field_latlon(q0, 'initial_q')
+plot_field_latlon(P, 'initial_P')
+plot_field_latlon(w, 'initial_w')
 
 # =======================================================================
 # Now we can timestep!
@@ -173,6 +204,8 @@ tol = 1e-3          # tolerance with which to compute steady state
 # timeloop
 while not_steady and t < tmax:
     t += dt
+    # update values in w
+    w.interpolate(w_expr)
     # compute h
     h_solver.solve()
     # compute u
@@ -185,6 +218,7 @@ while not_steady and t < tmax:
     q_solver.solve()
 
     print(f"at time {t}, change in h is: {norm(h-h0)}")
+    # print(assemble(w*dx))
     not_steady = norm(h-h0) > tol
 
     # update fields
@@ -192,8 +226,15 @@ while not_steady and t < tmax:
     u0.assign(u)
     q0.assign(q)
 
-plot_field_latlon(h)
-plot_field(q)
-plot_field(w)
-plot_field(P)
-plot_field(E)
+# =======================================================================
+# Now let's plot the final fields.
+plot_field_latlon(q, 'final_q')
+plot_field_latlon(h, 'final_h')
+plot_field_latlon(u, 'final_u')
+
+# =======================================================================
+# check that q has changed
+q_diff = Function(q0.function_space(), name='qdiff').assign(q-q_init)
+print_minmax(q_diff)
+plot_field_latlon(q_diff, "dq")
+
---
+++ plotting.py
@@ -0,0 +1,98 @@
+from tomplot import *
+import cartopy.crs as ccrs
+import numpy as np
+import matplotlib.pyplot as plt
+
+from firedrake import (trisurf, FunctionSpace, SpatialCoordinate, Function,
+                       pi, dot)
+from gusto import rotated_lonlatr_vectors, lonlatr_from_xyz
+
+
+def plot_field(field, figname):
+    # function to plot the field and save or show the plot
+    tsurf = trisurf(field)
+    plt.colorbar(tsurf)
+    plt.title(field.name())
+    plt.savefig(figname)
+
+
+def plot_field_latlon(field, figname):
+    # function to plot the field and save or show the plot
+
+    # We need to regrid onto lon-lat grid -- specify that here
+    lon_1d = np.linspace(-180.0, 180.0, 120)
+    lat_1d = np.linspace(-90, 90, 120)
+    lon_2d, lat_2d = np.meshgrid(lon_1d, lat_1d, indexing='ij')
+
+    projection=ccrs.Robinson()
+    contour_method = 'contour'
+
+    # set up figure
+    fig = plt.figure(figsize=(10, 5))
+    ax = fig.add_subplot(1, 1, 1, projection=projection)
+
+    # get function space and field data
+    if field.name() == 'u':
+      mesh = field.function_space().mesh()
+      V = FunctionSpace(mesh, "DG", 1)
+      xyz = SpatialCoordinate(mesh)
+      rotated_pole = (0.0, pi/2)
+      e_lon, e_lat, _ = rotated_lonlatr_vectors(xyz, rotated_pole)
+      u_zonal = Function(V).interpolate(dot(field, e_lon)).dat.data_ro
+      u_merid = Function(V).interpolate(dot(field, e_lat)).dat.data_ro
+      field_data = np.sqrt(u_zonal**2 + u_merid**2)
+    else:
+      V = field.function_space()
+      field_data = field.dat.data_ro
+
+    # get lat lon coordinates of DOFs
+    x, y, z = SpatialCoordinate(V.mesh())
+    lon, lat, _ = lonlatr_from_xyz(x, y, z)
+    coords_X = Function(V).interpolate(180.0 / np.pi * lon).dat.data_ro
+    coords_Y = Function(V).interpolate(180.0 / np.pi * lat).dat.data_ro
+
+    # map field data to regular lat-lon grid
+    field_data = regrid_horizontal_slice(
+        lon_2d, lat_2d, coords_X, coords_Y, field_data, periodic_fix='sphere'
+    )
+
+    # generate 10 contours between min and max values of field
+    contours = np.linspace(field_data.min(), field_data.max(), 10)
+
+    # plot contours
+    cmap, lines = tomplot_cmap(contours)
+    cf, _ = plot_contoured_field(
+        ax, lon_2d, lat_2d, field_data, contour_method, contours,
+        cmap=cmap, line_contours=lines, projection=projection
+    )
+
+    # add colourbar and title
+    add_colorbar_ax(
+        ax, cf, field.name(), location='bottom', cbar_labelpad=-10,
+    )
+    tomplot_field_title(ax, None, minmax=True, field_data=field_data)
+
+    # add quivers if field is velocity
+    if field.name() == 'u':
+        # Need to re-grid to lat-lon grid to get sensible looking quivers
+        regrid_zonal_data = regrid_horizontal_slice(
+            lon_2d, lat_2d, coords_X, coords_Y, u_zonal,
+            periodic_fix='sphere'
+        )
+        regrid_meridional_data = regrid_horizontal_slice(
+            lon_2d, lat_2d, coords_X, coords_Y, u_merid,
+            periodic_fix='sphere'
+        )
+        plt.quiver(lon_2d[::5], lat_2d[::5], regrid_zonal_data[::5]/field_data[::5], regrid_meridional_data[::5]/field_data[::5])
+        #plot_field_quivers(
+        #    ax, lon_2d, lat_2d, regrid_zonal_data, regrid_meridional_data,
+        #    magnitude_filter=1.0, scale=10.,
+        #    projection=ccrs.PlateCarree()
+        #)
+
+    plt.savefig(figname)
+
+
+def print_minmax(field):
+    # function to print the min and max of field in a nice way
+    print(f"min and max of {field.name()}: {field.dat.data.min()}, {field.dat.data.max()}")
